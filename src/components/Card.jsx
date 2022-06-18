8ff4e6f4252eb4fe500f6f1163515e8fd37e03fe
Yan Santos <49963127+yanlfs08@users.noreply.github.com>
2022-06-17 22:09:18 -0300

    Alterações Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,18 +9,27 @@ function Card({ data }) {
   } = data;
 
   return (
-    <div className="bg-white p-6 mt-10 rounded-lg shadow-md">
-      <div className="text-center">
-        <span className="block text-xl font-bold text-slate-700">{location.name}</span>
-        <span className="text-slate-400 text-sm font-medium">{`${location.region}, ${location.country}`}</span>
+    <div className="flex bg-white mt-10 rounded-lg shadow-md">
+      <div className="p-6">
+        <div className="text-center">
+          <span className="block text-xl font-bold text-slate-700">{location.name}</span>
+          <span className="text-slate-400 text-sm font-medium">{`${location.region}, ${location.country}`}</span>
+        </div>
+        <div className="font-bold text-slate-700 flex mt-4 mb-2">
+          <span className="text-8xl">{current.temp_c}</span>
+          <span className="text-2xl mt-3">°C</span>
+        </div>
       </div>
-      <div className="font-bold text-slate-700 flex mt-4 mb-2">
-        <span className="text-8xl">{current.temp_c}</span>
-        <span className="text-2xl mt-3">°C</span>
-      </div>
-      <div className="flex justify-center flex-col items-center ">
-        <img src={current.condition.icon} alt="" />
-        <span className="text-slate-700 font-medium">{current.condition.text}</span>
+      <div className="text-slate-700 font-medium justify-center items-center p-6 bottom-0">
+        <div className="flex justify-center flex-col items-center ">
+          <img src={current.condition.icon} alt="" />
+          <span>{current.condition.text}</span>
+          <div className="flex">
+            <span>{`Sensação térmica: ${current.wind_kph}`}</span>
+            <span className="text-xs">°C</span>
+          </div>
+          <span>{`Velocidade do vento: ${current.wind_kph} km/h`}</span>
+        </div>
       </div>
     </div>
   );
--- src/halpers/initialData.js
+++ src/halpers/initialData.js
@@ -10,6 +10,8 @@ const inicitialData = {
       text: '-',
       icon: '//cdn.weatherapi.com/weather/64x64/day/113.png',
     },
+    feelslike_c: 0,
+    wind_kph: 0,
   },
 };
 
